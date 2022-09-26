edd1261743f17a588b6274d1327241c8dfc6c4ff
HervalMata <hervalmataamparo@gmail.com>
2022-09-26 14:13:20 -0300

    feat:art: cris shopping - created bottom navigation bar

---
+++ lib/main_screens/customer_home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomerHomeScreen extends StatefulWidget {
+  const CustomerHomeScreen({super.key});
+
+  @override
+  State<CustomerHomeScreen> createState() => _CustomerHomeScreenState();
+}
+
+class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
+  int _selectedItem = 0;
+  final List<Widget> _tabs = const [
+    Center(
+      child: Text('Home'),
+    ),
+    Center(
+      child: Text('Categoria'),
+    ),
+    Center(
+      child: Text('Carrinho'),
+    ),
+    Center(
+      child: Text('Perfil'),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectedItem],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        selectedItemColor: Colors.pinkAccent,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedItem,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Categoria'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: 'Carrinho'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedItem = index;
+          });
+        },
+      ),
+    );
+  }
+}
